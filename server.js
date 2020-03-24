80ad274b79b4b70592aeca87e96278c3c16f7c83
HiufaiLau <hiuailau.41@gmail.com>
2020-03-24 01:07:37 +0100

    Updated the functions

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ io.on('connection', function(socket){
 });
   
 io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-      io.emit('chat message', msg);
-    });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
   });
+});
 
 http.listen(8080, function(){
   console.log('listening on *:8080');
