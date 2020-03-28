645f9360bc30445fa81f5ec2f1512b5e6f3a027f
HiufaiLau <hiuailau.41@gmail.com>
2020-03-28 19:40:57 +0100

    ADD readline-sync

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
 
+const x = 1
+const y = 2
+const z = 3
+console.count(
+  'The value of x is ' + x + 
+  ' and has been checked .. how many times?'
+)
+console.count(
+  'The value of x is ' + x + 
+  ' and has been checked .. how many times?'
+)
+console.count(
+  'The value of x is ' + x + 
+  ' and has been checked .. how many times?'
+)
+
+// console.log('\x1b[33m%s\x1b[0m', 'hi!')
+console.log('My %s is %d years old', 'cat', 2)
+const chalk = require('chalk')
+console.log(chalk.yellow('hi!'))
 // ----------------socket.io simple chat code----------------------
 // const app = require('express')();
 // const http = require('http').createServer(app);
@@ -30,28 +50,8 @@
 // });
 
 // -------------------HTTP GET request ----------------------------
-const https = require('https')
-const options = {
-  hostname: '127.0.0.1',
-    port: 5000
-    ,
-  path: '/',
-  method: 'GET'
-}
+// 
 
-const req = https.request(options, res => {
-  console.log(`statusCode: ${res.statusCode}`)
-
-  res.on('data', d => {
-    process.stdout.write(d)
-  })
-})
-
-req.on('error', error => {
-  console.error(error)
-})
-
-req.end()
 // -------------------simple Node.js code--------------------------
 // const http = require('http');
 // const hostname = '127.0.0.1';
@@ -85,13 +85,44 @@ req.end()
 
 
 
-//-------------------simple Node.js test 
+//-------------------simple Node.js test----------------------
 // const readline = require('readline').createInterface({
 //     input: process.stdin,
 //     output: process.stdout
 //   })
   
 //   readline.question(`What's your name?`, name => {
-//     console.log(`Hi ${name}!`)
+//     console.log(chalk.green(`Hi ${name}!`))
 //     readline.close()
 //   })
+
+// --------------------installed inquirer-------------=--------
+// const inquirer = require('inquirer')
+
+// var questions = [
+//   {
+//     type: 'input',
+//     name: 'name',
+//     message: "What's your name?"
+//   }
+// ]
+// inquirer.prompt(questions).then(answers => {
+//   console.log(chalk.yellow(`Hi ${answers['name']}!`))
+// })
+
+
+
+// ------------------with readline-sync------------------------
+// cant run nodemon !!!!!!!!!!
+
+let readlineSync = require('readline-sync');
+ 
+// Wait for user's response.
+let userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+ 
+// Handle the secret text (e.g. password).
+let favFood = readlineSync.question('What is your favorite food? ', {
+  hideEchoBack: true // The typed text on screen is hidden by `*` (default).
+});
+console.log('Oh, ' + userName + ' loves ' + favFood + '!');
